5ad60212142d3bb008fb88668e9b3334a8501be7
Nico Battaglia <nico-battaglia@hotmail.com>
2021-10-13 09:01:53 -0300

    Add functionality and separate in functions

--- index.js
+++ index.js
@@ -1,19 +1,73 @@
-function add(a,b) {
-    return a+b;
+// ==========================================
+// select buttons
+// ==========================================
+const buttons = document.querySelectorAll(".btn");
+const visorRow1 = document.querySelector(".row1");
+const visorRow2 = document.querySelector(".row2");
+const equal = document.querySelector(".equal");
+const AC = document.querySelector("#AC");
+const del = document.querySelector("#del");
+
+//===========================================
+// EventListeners
+// =========================================
+buttons.forEach((btn)=>btn.addEventListener("click", (e)=>{
+    showInDisplay(e)
+}));
+equal.addEventListener("click", operationFromVisor)
+AC.addEventListener("click", allClear)
+del.addEventListener("click", (e)=> deletePrevious(e))
+
+//===========================================
+// Functions
+// ==========================================
+let equalWasPreviousPress = null;
+
+function operationFromVisor() {
+    let operation = visorRow1.textContent
+    let result = eval(`${operation}`)
+    
+    // For numbers over 26 digits show scientific notation:
+    if (result.toLocaleString().length>26) {
+        visorRow2.textContent= result.toExponential()
+    } else {
+        visorRow2.textContent=result.toLocaleString('en-US', {maximumFractionDigits:16});
+    }
+    visorRow1.textContent=result
+    equalWasPreviousPress=true;
 }
 
-function subract(a,b) {
-    return a-b;
+function allClear() {
+    visorRow1.textContent="";
+    visorRow2.textContent="";
 }
 
-function multiply(a,b) {
-    return a*b;
+function showInDisplay(e) {
+    let pressed = e.target.textContent;
+    let classList = Array.from(e.target.classList)
+    let lastElement = Array.from(visorRow1.textContent).slice(-1)
+    
+    // Pressing '=' or 'del' do not add symbols to the display
+    if (pressed=="=" || pressed=="del") return
+
+    // If last key was "=" and next key is not an operator(+,-,*,/) to concatenate operations on the last result --> allClear() to start new calculations
+    if(equalWasPreviousPress && !classList.includes("operator")){
+        allClear()
+        equalWasPreviousPress=false
+    }
+    
+    //Pressing an operator twice only display the last one
+    if( classList.includes("operator") && (lastElement=="+" || lastElement=="-" || lastElement=="*" || lastElement=="/") ){
+        //replace last operator with the new one pressed:
+        let visorRow1Array = Array.from(visorRow1.textContent)
+        visorRow1Array.splice(-1,1,pressed)
+        return(visorRow1.textContent = visorRow1Array.join(""));
+    }
+    //Else just add content pressed
+    visorRow1.textContent+=pressed;
+    equalWasPreviousPress=false;
 }
 
-function divide(a,b){
-    return a/b;
-}
-
-function operate(operator, a, b) {
-    return operator(a,b)
+function deletePrevious(e) {
+    visorRow1.textContent = (Array.from(visorRow1.textContent).slice(0,-1)).join("")
 }
